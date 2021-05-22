9bc35d459cd36aefb0ba98ec42c4b6e3949d3213
apbprog <baltabaevar3@gmail.com>
2021-05-23 01:09:24 +0300

    task 3.1 is done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
 function compareArrays(arr1, arr2) {
   let result;
-
   // Ваш код
+  // Проверяем равенство длин массивов
+  if (arr1.length !== arr2.length) {
+    result = false;
+  } else {
+    // Для каждого элемента массива arr1 проверяем равен ли элемент массива arr2
+    result = arr1.every(function (item, index) {
+      for (let i = 0; i < arr2.length; i++) {
+        if (index === i && item === arr2[i]) {
+          return true;
+        }
+      }
+    })
+  }
 
   return result; // boolean
 }
