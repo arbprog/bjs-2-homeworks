c98b1a057ce3bc121564099de6748c099b60e2ae
apbprog <baltabaevar3@gmail.com>
2021-05-22 23:37:40 +0300

    tasks 2.1 and 2.2 is done

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,26 +1,57 @@
+"use strict";
+
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
-
+  let min,
+    max,
+    sum = 0,
+    avg = 0;
   // Ваш код
+  //Обходим массив
+  for (let prop in arr) {
+    //Если значение меньше min, то это значение записываем в min
+    if (arr[prop] === Math.min(...arr)) {
+      min = arr[prop];
+    }
+    //Если значение больше max, то это значение записываем в max
+    if (arr[prop] === Math.max(...arr)) {
+      max = arr[prop];
+    }
+    //Находим сумму всех элементов массива
+    sum += arr[prop];
+  }
+  //Находим среднее значение массива с округлением до 2х значений после запятой
+  avg = (sum / arr.length).toFixed(2);
 
-  return { min:min, max:max, avg:avg };
+  return {
+    min: min,
+    max: max,
+    avg: avg
+  };
 }
 
 // Задание 2
+//Функция, которая находит суммы всех элементов массива
 function worker(arr) {
-  let sum;
-
+  let sum = 0;
   // Ваш код
-
+  
+  for (let prop in arr) {
+    sum += arr[prop];
+  }
   return sum;
 }
 
+//Функция, которая находит наибольшее значение элементов массива
 function makeWork(arrOfArr, func) {
-  let max
-
-  // Ваш кода
+  let max = 0;
+  // Ваш код
   // for ...
-  
-  return max
+  for (let prop in arrOfArr) {
+    if(worker(arrOfArr[prop]) > max) {
+      max = worker(arrOfArr[prop]);
+    }
+  }
+
+  return max;
 }
