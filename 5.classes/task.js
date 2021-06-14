4c89d6f9a4afdce9559050b870de40abe4eff24f
apbprog <baltabaevar3@gmail.com>
2021-06-15 01:02:23 +0300

    task 5.1 is edited

---
+++ 5.classes/task.js
@@ -0,0 +1,31 @@
+"use strict";
+
+class PrintEditionItem {
+
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.state = 100;
+        this.type = null;
+    }
+
+    fix() {
+        this.state *= 1.5;
+    }
+
+    get state() {
+        return this._state;
+    }
+
+    set state(value) {
+        if (value < 0) {
+            this._state = 0;
+        } else if (value > 100) {
+            this._state = 100;
+        } else {
+            this._state = value;
+        }
+    }
+
+}
