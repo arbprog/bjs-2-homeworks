47787fac46c19fe1b3567a3a8ffd6889dcceff4f
apbprog <baltabaevar3@gmail.com>
2021-06-15 12:57:50 +0300

    task 5.1 is edited

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -31,46 +31,71 @@ class PrintEditionItem {
 }
 
 class Magazine extends PrintEditionItem {
-    constructor() {
-        super();
+    constructor(name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
         this.type = "magazine";
     }
 }
 
 class Book extends PrintEditionItem {
-    constructor(author) {
-        super();
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
         this.author = author;
-        this.type = "book";
+        this.type = "book"; 
     }
 }
 
 class NovelBook extends Book {
-    constructor() {
-        super();
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
         this.type = "novel";
     }
 }
 
 class FantasticBook extends Book {
-    constructor() {
-        super();
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
         this.type = "fantastic";
     }
 }
 
 class DetectiveBook extends Book {
-    constructor() {
-        super();
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
         this.type = "detective";
     }
 }
 
-const picknick = new Book("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168);
 
-console.log(picknick.author); //"Аркадий и Борис Стругацкие"
-console.log(picknick.name);
-picknick.state = 10;
-console.log(picknick.state); //10
-picknick.fix();
-console.log(picknick.state);
+class Library {
+    constructor(name = "", books = []) {
+        this.name = name;
+        this.books = books;
+    }
+
+    addBook(book) {
+        if(book._state > 30) {
+            this.books.push(book);
+        } 
+    }
+
+    findBookBy(type, value) {
+        if(type) {
+            return value;
+        } else {
+            return null;
+        }
+    }
+}
+
+
+
+
+
+const library = new Library("Библиотека имени Ленина");
+library.addBook(new DetectiveBook("Артур Конан Дойл", "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008));
+library.addBook(new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168));
+library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+library.addBook(new Magazine("Мурзилка", 1924, 60));
+
+console.log(library.books[2]);
