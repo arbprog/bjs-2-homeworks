f844ae3b382ef11e83aebec28c6389d524d89577
arbprog <baltabaevar3@gmail.com>
2021-06-15 14:09:10 +0300

    task 5.1 is edited

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -81,7 +81,7 @@ class Library {
 
     findBookBy(type, value) {
         if(type) {
-            return value;
+            return book;
         } else {
             return null;
         }
@@ -93,9 +93,13 @@ class Library {
 
 
 const library = new Library("Библиотека имени Ленина");
+
 library.addBook(new DetectiveBook("Артур Конан Дойл", "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008));
 library.addBook(new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168));
 library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
 library.addBook(new Magazine("Мурзилка", 1924, 60));
 
-console.log(library.books[2]);
+console.log(library.findBookBy("name", "Властелин колец")); //null
+console.log(library.findBookBy("releaseDate", 1924).name); //"Мурзилка"
+
+
