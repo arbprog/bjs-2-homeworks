694227387950267a80ea6d6ce9376fc78a238398
arbprog <baltabaevar3@gmail.com>
2021-06-15 11:24:32 +0300

    task 5.1 is edited

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -29,3 +29,48 @@ class PrintEditionItem {
     }
 
 }
+
+class Magazine extends PrintEditionItem {
+    constructor() {
+        super();
+        this.type = "magazine";
+    }
+}
+
+class Book extends PrintEditionItem {
+    constructor(author) {
+        super();
+        this.author = author;
+        this.type = "book";
+    }
+}
+
+class NovelBook extends Book {
+    constructor() {
+        super();
+        this.type = "novel";
+    }
+}
+
+class FantasticBook extends Book {
+    constructor() {
+        super();
+        this.type = "fantastic";
+    }
+}
+
+class DetectiveBook extends Book {
+    constructor() {
+        super();
+        this.type = "detective";
+    }
+}
+
+const picknick = new Book("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168);
+
+console.log(picknick.author); //"Аркадий и Борис Стругацкие"
+console.log(picknick.name);
+picknick.state = 10;
+console.log(picknick.state); //10
+picknick.fix();
+console.log(picknick.state);
